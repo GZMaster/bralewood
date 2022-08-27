d4983b6b261ac4b3b8c2d3b8d3c455a8043bc8c6
GZMaster <danigamester@gmail.com>
2022-08-27 16:21:08 +0100

    email.js

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,37 +1,73 @@
-import React from "react";
+import React, { useState} from "react";
+import { send } from 'emailjs-com';
 import "./Contact.css";
 import "../services/Services.css";
 import { FaPhoneAlt, FaLocationArrow } from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
 
 const Contact = () => {
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    to_name: 'Bralewood',
+    message: '',
+    reply_to: 'danigamester@gmail',
+    from_email: '',
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send(
+      'service_hhl8ldq',
+      'template_djummb7',
+      toSend,
+      'IxwXkIwaa63vnsgkR'
+    )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="C_Container">
       <div className="C_Wrapper">
         <section className="Contact_Wrapper">
-          <div className="C_Form">
+          <form className="C_Form" onSubmit={onSubmit}>
             <h4 className="C_title">Contact Us</h4>
             <input
               type="text"
               placeholder="NAME"
-              name="name"
+              name="from_name"
+              value={toSend.from_name}
               className="form_control"
+              onChange={handleChange}
             />
             <input
               type="email"
               placeholder="EMAIL"
-              name="email"
+              name="from_email"
+              value={toSend.from_email}
               className="form_control"
+              onChange={handleChange}
             />
             <textarea
               name="message"
               placeholder="MESSAGE"
+              type="text"
               cols="30"
               rows="10"
+              value={toSend.message}
               className="form_control"
+              onChange={handleChange}
             ></textarea>
-            <button className="C_btn">Submit Here</button>
-          </div>
+            <button type="submit" className="C_btn">Submit Here</button>
+          </form>
           <div className="C_Info">
             <div className="C_item">
               <h3 className="C_item_title">
