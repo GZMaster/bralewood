8d464fd98d3daa08ba2b037c28e2501afcf226c7
Ghost1706 <56081021+Ghost1706@users.noreply.github.com>
2022-09-03 20:57:11 +0100

    Contact Update

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,35 +1,31 @@
-import React, { useState} from "react";
-import { send } from 'emailjs-com';
-import "./Contact.css";
+import React, { useState } from "react";
+import { send } from "emailjs-com";
+import "./Contact.scss";
 import "../services/Services.css";
-import { FaPhoneAlt, FaLocationArrow } from "react-icons/fa";
-import { IoMdMail } from "react-icons/io";
-import SuccessfulNotification from "../../components/notification/SuccessfulNotification"
+import { AiOutlineMail } from "react-icons/ai";
+import { FiPhoneCall } from "react-icons/fi";
+import { IoLocationOutline } from "react-icons/io5";
+import SuccessfulNotification from "../../components/notification/SuccessfulNotification";
 
 const Contact = () => {
   const [toSend, setToSend] = useState({
-    from_name: '',
-    to_name: 'Bralewood',
-    message: '',
-    reply_to: 'info@bralewood.com',
-    from_email: '',
+    from_name: "",
+    to_name: "Bralewood",
+    message: "",
+    reply_to: "info@bralewood.com",
+    from_email: "",
   });
 
   const onSubmit = (e) => {
     e.preventDefault();
-    send(
-      'service_hhl8ldq',
-      'template_djummb7',
-      toSend,
-      'IxwXkIwaa63vnsgkR'
-    )
+    send("service_hhl8ldq", "template_djummb7", toSend, "IxwXkIwaa63vnsgkR")
       .then((response) => {
-        console.log('SUCCESS!', response.status, response.text);
+        console.log("SUCCESS!", response.status, response.text);
       })
       .catch((err) => {
-        console.log('FAILED...', err);
+        console.log("FAILED...", err);
       });
-      
+
     SuccessfulNotification("Mail Sent Successfully");
   };
 
@@ -40,28 +36,43 @@ const Contact = () => {
   return (
     <div className="C_Container">
       <div className="C_Wrapper">
+        <h4 className="C_title">Contact Us</h4>
+        <p className="C_Text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nulla
+          totam hic aspernatur vel quia ipsa distinctio repudiandae nostrum ea?
+        </p>
         <section className="Contact_Wrapper">
           <form className="C_Form" onSubmit={onSubmit}>
-            <h4 className="C_title">Contact Us</h4>
+            <div className="C_Flex">
+              <input
+                type="text"
+                placeholder="Your Full Name"
+                name="from_name"
+                value={toSend.from_name}
+                className="form_control"
+                onChange={handleChange}
+              />
+              <input
+                type="email"
+                placeholder="your email address"
+                name="from_email"
+                value={toSend.from_email}
+                className="form_control"
+                onChange={handleChange}
+              />
+            </div>
             <input
               type="text"
-              placeholder="NAME"
-              name="from_name"
-              value={toSend.from_name}
-              className="form_control"
-              onChange={handleChange}
-            />
-            <input
-              type="email"
-              placeholder="EMAIL"
-              name="from_email"
+              placeholder="your subject"
+              name="from_subject"
               value={toSend.from_email}
               className="form_control"
               onChange={handleChange}
             />
+
             <textarea
               name="message"
-              placeholder="MESSAGE"
+              placeholder="Message"
               type="text"
               cols="30"
               rows="10"
@@ -69,13 +80,15 @@ const Contact = () => {
               className="form_control"
               onChange={handleChange}
             ></textarea>
-            <button type="submit" className="C_btn">Submit Here</button>
+            <button type="submit" className="C_btn">
+              Send Message
+            </button>
           </form>
           <div className="C_Info">
             <div className="C_item">
               <h3 className="C_item_title">
-                <FaLocationArrow />
-                <span> Address</span>
+                <IoLocationOutline />
+                <span>Our Address</span>
               </h3>
               <h3 className="C_item-text">
                 7th floor CBC Towers, No 11 Olubunmi Owa Street, Lekki Phase 1,
@@ -84,15 +97,15 @@ const Contact = () => {
             </div>
             <div className="C_item">
               <h3 className="C_item_title">
-                <IoMdMail />
-                <span> Email</span>
+                <AiOutlineMail />
+                <span> Email Us</span>
               </h3>
               <h3 className="C_item-text">info@bralewood.com</h3>
             </div>
             <div className="C_item">
               <h3 className="C_item_title">
-                <FaPhoneAlt />
-                <span> Telephone</span>
+                <FiPhoneCall />
+                <span>Call Us</span>
               </h3>
               <h3 className="C_item-text">01-4545505</h3>
             </div>
