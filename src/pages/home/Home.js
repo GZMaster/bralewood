a8585caa74635ff593019903926711347ef4faa9
GZMaster <danigamester@gmail.com>
2022-09-05 01:39:56 +0100

    home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./Home.scss";
+import Banner from "../../components/banner/Banner";
 import About from "../about/About";
 import Contact from "../contact/Contact";
 import Services from "../services/Services";
-import Management from "../management/Management";
 
 const Home = () => {
   return (
     <div className="home__Container">
-      {/* <About /> */}
+      <Banner className="home_banner_start"/>
+      <About />
+      <Services />
       <Contact />
     </div>
   );
