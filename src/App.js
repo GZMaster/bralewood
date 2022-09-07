c67b5f41741bf27a4e76cc22a0c98f63633ed526
GZMaster <danigamester@gmail.com>
2022-09-07 16:15:14 +0100

    NAVIGATION

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import NavBar from "./components/navbar/NavBar";
 import Footer from "./components/footer/Footer";
 import "./App.css";
 
 function App() {
-  console.log(useLocation);
-
   return (
     <div className="App">
       <NavBar />
