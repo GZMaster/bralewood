dcc9354a69129222d09b34e223feae8e618cf7fe
Ghost1706 <56081021+Ghost1706@users.noreply.github.com>
2022-09-07 08:30:05 +0100

    Sticky Nav

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Footer from "./components/footer/Footer";
 function App() {
   return (
     <div className="App">
-      <div className="NavBar">
+      <div>
         <NavBar className="navbar" />
       </div>
 
