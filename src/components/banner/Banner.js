d9b3ac3301f448403bc2a1479ef2d599fc1b08b4
GZMaster <33158650+GZMaster@users.noreply.github.com>
2022-11-17 15:03:03 +0100

    home page bug resolve

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -6,15 +6,17 @@ const Banner = () => {
   return (
     <div className="banner_container">
       <div className="banner_wrapper">
-        <h3>
-          World-class financial services, We are <b>BRALEWOOD.</b>
-        </h3>
-        <p>
-          Here at Bralewood, we’re committed to doing all we can to ensure you
-          have the best experience with the variety of financial services we
-          offer. Some of which include; foreign exchange transactions, business
-          consulting, cash deliveries and many more.
-        </p>
+        <div className="banner_text">
+          <h3>
+            World-class financial services, We are <b>BRALEWOOD.</b>
+          </h3>
+          <p>
+            Here at Bralewood, we’re committed to doing all we can to ensure you
+            have the best experience with the variety of financial services we
+            offer. Some of which include; foreign exchange transactions,
+            business consulting, cash deliveries and many more.
+          </p>
+        </div>
         <Link activeClass="active" smooth spy to="contact">
           Contact Us
         </Link>
